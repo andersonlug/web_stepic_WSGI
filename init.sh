9b93359bdaa103e3654d92b6649b53ab53bb8ffc
achicha <meth787@gmail.com>
2016-03-07 18:27:53 +0300

    fix nginx

--- init.sh
+++ init.sh
@@ -3,3 +3,5 @@ sudo mv /home/box/web/etc/nginx.conf  /etc/nginx/nginx.conf
 sudo /etc/init.d/nginx restart
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
+sudo gunicorn -c gunicorn_test_config.py hello:app
+
